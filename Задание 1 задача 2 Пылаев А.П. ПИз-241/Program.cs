568aa5b7e68e6139d4fd549a8f59ece852f3b057
PylaevAleksandr <pylaev.aleksandr.99@mail.ru>
2025-04-20 18:39:27 +0700

    Add files via upload

---
+++ Задание 1 задача 2 Пылаев А.П. ПИз-241/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите число x (не менее 100): ");
+        string x = Console.ReadLine();                            // Добавить проверку числа x (больше ли оно)
+                                                                  
+        char secondDigit = x[1];                                  // выбираем вторую цифру числа x
+                                
+        string numberWithoutSecond = x[0] + x.Substring(2);       // удаляем вторую цифру из начального числа 
+                                                           
+        string yString = numberWithoutSecond + secondDigit;       // генерируем число y, добавляя удаленную цифру в конце
+                                                           
+        int y = int.Parse(yString);                               // конвертируем строку в число
+
+        Console.WriteLine("Значение числа n: " + y);
+    }
+}
